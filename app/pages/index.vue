6f69da0dfa925a3950ca0e6a87ee7c18d16f9d77
nec-tanabiki <t-tanabiki@nec.com>
2026-03-30 13:51:49 +0900

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,4 @@
+<template></template>
 <script setup>
-  definePageMeta({
-    // define middleware as a function
-    middleware: [
-      function (to, from) {
-        const {status} = useAuth();
-        console.log(`pages_index get auth status : ${status.value}`);
-        if (status.value === 'authenticated') {
-          // ログイン後はairwayStatusへリダイレクト
-          console.log(`pages_index redirect airwayStatus.`);
-          return navigateTo('/airwayStatus');
-        }
-      }
-    ],
-  })
+  await navigateTo('/login');
 </script>
